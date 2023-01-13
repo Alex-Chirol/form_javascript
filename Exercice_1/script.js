9df631dee26c1f8f87521eee6cd859e231cf7af3
alexchirol <alexandrechirol62@gmail.com>
2023-01-13 13:42:28 +0100

    Finalisation de l'exercice 1

--- Exercice_1/script.js
+++ Exercice_1/script.js
@@ -1,6 +1,6 @@
 const contactForm = document.getElementById('contactForm');
 
-contactForm.addEventListener('submit', (event) => {
+contactForm.addEventListener('submit', async (event) => {
     event.preventDefault();
 
     const firstNameInput = document.getElementById("firstName");
@@ -37,37 +37,36 @@ contactForm.addEventListener('submit', (event) => {
     phoneError.style.display = 'none';
     passwordError.style.display = 'none';
 
-    if (!formData.firstName || !formData.lastName || !formData.email || !formData.phone || !formData.password) {
-        const emailRegex = /[A-Z0-9._%+-]+@[A-Z0-9-]+.+.[A-Z]{2,4}/igm;
-        const nameRegex = /^[a-zA-Z ]+$/
-        const phoneRegex = /^(?:(?:\+|00)33|0)\s*[1-9](?:[\s.-]*\d{2}){4}$/
-    
-        if (!formData.firstName || !nameRegex.test(formData.firstName)) {
-            
-            errors.firstName = true;
-            firstNameError.style.display = 'block';
-        }
-        if (!formData.lastName || !nameRegex.test(formData.lastName)) {
-            errors.lastName = true;
-            lastNameError.style.display = 'block';
-        }
-        if (!formData.email || !emailRegex.test(formData.email)) {
-            errors.email = true;
-            emailError.style.display = 'block';
-        }
-        if (!formData.phone || !phoneRegex.test(formData.phone)) {
-            errors.phone = true;
-            phoneError.style.display = 'block';
-        }
-        if (!formData.password || formData.password.length < 8) {
-            errors.password = true;
-            passwordError.style.display = 'block';
-        }
+    const emailRegex = /[A-Z0-9._%+-]+@[A-Z0-9-]+.+.[A-Z]{2,4}/igm;
+    const nameRegex = /^[a-zA-Z ]+$/;
+    const phoneRegex = /^(?:(?:\+|00)33|0)\s*[1-9](?:[\s.-]*\d{2}){4}$/;
+    const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[A-Za-z\d$@$!%*?&_]{8,50}$/;
+
+    if (!formData.firstName || !nameRegex.test(formData.firstName)) {
+        errors.firstName = true;
+        firstNameError.style.display = 'block';
+    }
+    if (!formData.lastName || !nameRegex.test(formData.lastName)) {
+        errors.lastName = true;
+        lastNameError.style.display = 'block';
+    }
+    if (!formData.email || !emailRegex.test(formData.email)) {
+        errors.email = true;
+        emailError.style.display = 'block';
+    }
+    if (!formData.phone || !phoneRegex.test(formData.phone)) {
+        errors.phone = true;
+        phoneError.style.display = 'block';
+    }
+    if (!formData.password || !passwordRegex.test(formData.password)) {
+        errors.password = true;
+        passwordError.style.display = 'block';
     }
 
     if (!Object.values(errors).includes(true)) {
         console.log(formData)
     }
-
+    
+    contactForm.reset();
 
 })
