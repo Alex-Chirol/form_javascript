907df1d682e5707a33ca73a282ad0121f3a238bc
alexchirol <alexandrechirol62@gmail.com>
2023-01-14 01:53:33 +0100

    Finalisation de l'exercice 2

--- Exercice_2/script.js
+++ Exercice_2/script.js
@@ -6,15 +6,15 @@ contactForm.addEventListener('submit', async (event) => {
     const nameInput = document.getElementById("name");
     const adressInput = document.getElementById("adress");
     const phoneInput = document.getElementById("phone");
-    const selectPizza = document.getElementById("selectPizza");
     const selectSize = document.getElementById("selectSize");
+    const selectPizza = document.getElementById("selectPizza");
 
     const formData = {
         name: nameInput.value,
         adress: adressInput.value,
         phone: phoneInput.value,
+        selectSize: selectSize.value,
         selectPizza: selectPizza.value,
-        selectSize: selectSize.value
     }
 
     const errors = {
@@ -28,14 +28,14 @@ contactForm.addEventListener('submit', async (event) => {
     const nameError = document.getElementById('nameError');
     const adressError = document.getElementById('adressError');
     const phoneError = document.getElementById('phoneError');
-    const selectPizzaError = document.getElementById('selectPizzaError');
     const selectSizeError = document.getElementById('selectSizeError');
+    const selectPizzaError = document.getElementById('selectPizzaError');
 
     nameError.style.opacity = '0';
     adressError.style.opacity = '0';
     phoneError.style.opacity = '0';
-    selectPizzaError.style.opacity = '0';
     selectSizeError.style.opacity = '0';
+    selectPizzaError.style.opacity = '0';
 
     const phoneRegex = /^(?:(?:\+|00)33|0)\s*[1-9](?:[\s.-]*\d{2}){4}$/;
     const nameRegex = /^[a-zA-Z ]+$/;
@@ -52,22 +52,48 @@ contactForm.addEventListener('submit', async (event) => {
         errors.phone = true;
         phoneError.style.opacity = '1';
     }
-    if (!formData.selectPizza) {
-        errors.selectPizza = true;
-        selectPizzaError.style.opacity = '1';
-    }
     if (!formData.selectSize) {
         errors.selectSize = true;
         selectSizeError.style.opacity = '1';
     }
+    if (!formData.selectPizza) {
+        errors.selectPizza = true;
+        selectPizzaError.style.opacity = '1';
+    }
+
+    const sizePrice = {
+        Small: 5,
+        Medium: 8,
+        Large: 10
+    };
+
+    const pizzaPrice = {
+        "Pizza Norvégienne": 2,
+        "Pizza Bianca": 3,
+        "Pizza Savoyarde": 4,
+        "Pizza bolognaise": 1,
+        "Pizza Pepperoni": 2
+    };
+
+    const price = sizePrice[selectSize.value] + pizzaPrice[selectPizza.value];
+
+    const formCancel = document.getElementById('formCancel');
+
+    formCancel.style.display = 'none';
 
     if (!Object.values(errors).includes(true)) {
         console.log(formData)
+
+        formCancel.style.display = 'block';
+        contactForm.style.display = 'none';
+        choiseText.innerHTML = `${selectPizza.value} ${selectSize.value}`;
+        priceText.innerHTML = `${price} €`;
+
+
+
     }
-    
-    contactForm.reset();
 
 })
 
-  
+
 
